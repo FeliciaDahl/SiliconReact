d9e9ce56b6887ae8903dea8b055dd349062c1333
Felicia Dahl <feliciadahl01@gmail.com>
2024-11-13 12:45:07 +0100

    Added folder styles for Css code

--- src/pages/Home/components/Subscribe/Newsletter.jsx
+++ src/pages/Home/components/Subscribe/Newsletter.jsx
@@ -2,16 +2,12 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <>
   
     <div className="subscribe flex-1 pb-1">
-        <img src="./images/contact/notification.svg" alt="picture of a bell"/>
+        <img src="./images/Contact/notification.svg" alt="picture of a bell"/>
         <h3 className="h3-24">Subscribe to our newsletter</h3>
     </div>
 
-
-
-</>
   )
 }
 
